2770ce515f2ffcbf5b5bc3bde728e54840bd5cb6
plasilva1997 <contact@plasilva.com>
2022-05-12 19:22:14 +0200

    :sparkles: login ok avec stock en local storage token :lipstick: Ui/Ux login form

--- dist/assets/js/background.js
+++ dist/assets/js/background.js
@@ -1,14 +1,13 @@
 /*POUR DOM DE LA PAGE*/
 
-const url =window.location.toString()
+const url = window.location.toString()
 
-    // use `url` here inside the callback because it's asynchronous!
-    if(url === "https://www.caf.fr/") {
-        let div = document.createElement("h1");
-        document.body.insertBefore(div, document.body.firstChild);
+// use `url` here inside the callback because it's asynchronous!
+if (url === "https://www.caf.fr/") {
+    let div = document.createElement("div");
+    document.body.insertBefore(div, document.body.firstChild);
 
-        div.innerText = "JE SUIS UNE DIV";
-        div.style.color="red";
-    }
+    div.innerText = "Hello World!";
+    div.style.color = "red";
+}
 
-localStorage.setItem("chatte","dsfsdf")
--- dist/assets/js/popup.js
+++ dist/assets/js/popup.js
@@ -1,3 +1,23 @@
 /*POUR L'HTML DE L'EXETENSION */
 
-localStorage.setItem("bite","dddddd")
+document.getElementById("submit").addEventListener("click", login);
+
+function login() {
+    var login = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    fetch("https://chympy.net/api/particuliers/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: login,
+            password: password
+        })
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        localStorage.setItem("token", data.token);
+    });
+}
