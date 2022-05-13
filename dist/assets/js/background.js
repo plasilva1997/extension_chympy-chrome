3fac9f624da36fe446a62d3f1063c2c79c83f6de
plasilva1997 <contact@plasilva.com>
2022-05-13 15:47:17 +0200

    :bug: fix banner

--- dist/assets/js/background.js
+++ dist/assets/js/background.js
@@ -34,7 +34,7 @@ chrome.storage.local.get(["company","token"], function(items) {
                 }
 
                 div.id = 'extention_chympy_patern'; //ajout d'un id à la div
-                div.innerHTML = '<div style="width: 100%; border-bottom: #FD9F57 2px solid; height: auto; min-height: 5vh; color: white; background-color: #fafafa; display: flex; justify-content: space-around; align-content: center; align-items: center">' +
+                div.innerHTML = '<div style="z-index: 9999999999999; position: relative;width: 100%; border-bottom: #FD9F57 2px solid; height: auto; min-height: 5vh; color: white; background-color: #fafafa; display: flex; justify-content: space-around; align-content: center; align-items: center">' +
                     '<div style="background-repeat: no-repeat; background-size: contain; background-position: center;background-image: url(https://i.imgur.com/E2qN8Da.png); width: 150px; height: 100px"></div>' +
                     '<p style="color: #4b4b4b; font-size: 22px; font-weight: 700; margin: 0; width: 70%;line-height: 1.5em">Ce site est partenaire de Chympy, '+ freeCondition +' vous recuperez '+ cashback +' '+ unitCashback +' en CashBack. '+ freeConditonMinCashBack +' (cliquer <a style="font-weight: 900; text-decoration: none; color: #FD9F57" href="https://www.chympy.net/cgu" target="_blank">ici</a> pour voir les conditions d\'utilisation)</p>' +
                     '</div>'; //ajout d'un contenu à la div*
