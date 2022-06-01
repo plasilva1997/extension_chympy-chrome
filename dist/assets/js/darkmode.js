24cd29f9de5fe03ee92ca6f3ac4fd763e6c6e2ca
plasilva1997 <contact@plasilva.com>
2022-06-01 17:16:53 +0200

    :lipstick: Darkmode done and navbar ok

--- dist/assets/js/darkmode.js
+++ dist/assets/js/darkmode.js
@@ -1,6 +1,8 @@
 let darkMode = document.getElementById("darkmode"); //recuperation du bouton darkmode
 darkMode.addEventListener("click", dark_mode); //ajout d'un evenement au click du bouton darkmode
 let checkDarkMode = localStorage.getItem("darkMode"); //recuperation du darkmode dans le localstorage
+let lougoutIcon = document.getElementById("logout"); //recuperation du bouton logout
+
 
 function dark_mode() {
     checkDarkMode = localStorage.getItem("darkMode"); //recuperation du darkmode dans le localstorage
@@ -16,9 +18,16 @@ function setDarkmode() {
     checkDarkMode = localStorage.getItem("darkMode");
     if (checkDarkMode === null || checkDarkMode === "false") {
         document.body.classList.remove("dark-mode");
+        darkMode.classList.remove("icon_on");
+        darkMode.classList.add("icon_off");
+        lougoutIcon.classList.remove("lougout-dm-actif");
+
 
     } else {
         document.body.classList.add("dark-mode");
+        darkMode.classList.remove("icon_off");
+        darkMode.classList.add("icon_on");
+        lougoutIcon.classList.add("lougout-dm-actif");
     }
 }
 
